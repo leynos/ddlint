8fb9ecefcce46f293b3be26a4f5f4c66465ec3b8
Leynos <leynos@troubledskies.net>
2025-09-07 03:33:36 +0100

    Label postfix expression test cases

--- tests/expression_postfix.rs
+++ tests/expression_postfix.rs
@@ -9,12 +9,12 @@ use ddlint::test_util::{bit_slice, field_access, lit_num, method_call, tuple_ind
 use rstest::rstest;
 
 #[rstest]
-#[case("foo.bar(x)", method_call(var("foo"), "bar", vec![var("x")]))]
-#[case("foo.bar", field_access(var("foo"), "bar"))]
-#[case("e[1,0]", bit_slice(var("e"), lit_num("1"), lit_num("0")))]
-#[case("t.0", tuple_index(var("t"), "0"))]
-#[case("foo.bar(1, 2)", method_call(var("foo"), "bar", vec![lit_num("1"), lit_num("2")]))]
-#[case("foo.bar().baz[1,0]", bit_slice(field_access(method_call(var("foo"), "bar", vec![]), "baz"), lit_num("1"), lit_num("0")))]
+#[case::method_call("foo.bar(x)", method_call(var("foo"), "bar", vec![var("x")]))]
+#[case::field_access("foo.bar", field_access(var("foo"), "bar"))]
+#[case::bit_slice("e[1,0]", bit_slice(var("e"), lit_num("1"), lit_num("0")))]
+#[case::tuple_index("t.0", tuple_index(var("t"), "0"))]
+#[case::multi_arg_call("foo.bar(1, 2)", method_call(var("foo"), "bar", vec![lit_num("1"), lit_num("2")]))]
+#[case::chained_postfix("foo.bar().baz[1,0]", bit_slice(field_access(method_call(var("foo"), "bar", vec![]), "baz"), lit_num("1"), lit_num("0")))]
 fn parses_postfix_expressions(#[case] src: &str, #[case] expected: Expr) {
     let expr = parse_expression(src).unwrap_or_else(|e| panic!("source {src:?} errors: {e:?}"));
     assert_eq!(expr, expected);
